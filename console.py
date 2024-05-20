e8784c477f617b34cb12e3ad2e76fe817aeb734d
Macaulay Israel <israelmacaulay8@gmail.com>
2024-05-19 21:52:41 -0700

    work hard

--- console.py
+++ console.py
@@ -38,6 +38,7 @@ class HBNBCommand(cmd.Cmd):
     lst_str = storage.all()
     prompt = "(hbnb)"
     model_lst = ["BaseModel"]
+
     def do_create(self, line):
         """ A mehod that print a repesentation of the instance.
 
@@ -72,6 +73,55 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
 
     def do_destroy(self, line):
+        lst = line.split()
+        if len(lst) < 1:
+            print("** class name missing **")
+        elif lst[0] not in HBNBCommand.model_lst:
+            print("** class doesn't exist **")
+        elif len(lst) < 2:
+            print("** instance id missing **")
+        else:
+            inst = lst[0] + "." + lst[1]
+            temp = HBNBCommand.lst_str.get(inst)
+            if inst:
+                del HBNBCommand.lst_str[inst]
+                print(HBNBCommand.lst_str)
+            else:
+                print("** no instance found **")
+
+    def do_all(self, line):
+        lst = []
+        line = line.split()
+        if len(line) < 1:
+            print("** class doesn't exist **")
+        elif line[0] not in HBNBCommand.model_lst:
+            print("** class doesn't exist **")
+        else:
+            for i in HBNBCommand.lst_str:
+                lst.append(HBNBCommand.lst_str[i].__str__())
+        print(lst)
+
+    def do_update(self, line):
+        lst = line.split()
+        if len(lst) < 1:
+            print("** class name missing **")
+        elif lst[0] not in HBNBCommand.model_lst:
+            print("** class doesn't exist **")
+        elif len(lst) < 2:
+            print("** instance id missing **")
+        else:
+            inst = lst[0] + "." + lst[1]
+            temp = HBNBCommand.lst_str.get(inst)
+            if inst:
+                if len(lst) < 3:
+                    print("** attribute name missing **")
+                elif (hasattr(temp, "first_name")):
+                    print(lst[2])
+                else:
+                    print("** value missing **")
+            else:
+                print("** no instance found **")
+
     def do_quit(self, rest):
         """Exit the program"""
         return True
